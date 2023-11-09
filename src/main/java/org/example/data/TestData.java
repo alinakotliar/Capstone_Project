6fa9d2bb3096d7444c6ed2a42cc7e97da04a02bf
alinakotliar <136513876+alinakotliar@users.noreply.github.com>
2023-11-09 11:41:22 +0400

    Update TestData.java

--- src/main/java/org/example/data/TestData.java
+++ src/main/java/org/example/data/TestData.java
@@ -1,7 +1,7 @@
 package org.example.data;
 
 public class TestData {
-    public static final String email = "alina.kotliar.personal@gmail.com";
-    public static final String password = "AccountforTest5!";
+    public static final String email = "";
+    public static final String password = "";
     public static final String searchQuery = "Whitney Houston";
 }
